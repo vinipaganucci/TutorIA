504b84252a7f8f1767fb7b7d94836141549cdd41
Vinicius Giovanne Paganucci Vidotto <98669129+vinipaganucci@users.noreply.github.com>
2025-09-05 11:10:20 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,18 @@
 
+window.addEventListener('load', () => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            console.log('SW registration failed');
+        }
+    }
+
+}
 
 const botao = document.getElementById('entrar');
 
---
+++ sw.js
@@ -0,0 +1,57 @@
+const cacheName = 'tutoria'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './manifest.webmanifest',
+                './login.js',
+                './styles.css',
+                './login.html',
+                './history.html',
+                './math.html',
+                './portuguese.html',
+                './history.js',
+                './math.js',
+                './portuguese.js',
+                './img/97b479d1-7a48-4124-a408-76570c6ebab2.png',
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
+}
